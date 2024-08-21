3b4e6198937dd359728ee53fbe52d6fc00ea3f01
Ye Won Lee <yewon.lee@seavantage.com>
2024-08-21 17:22:16 +0900

    [impv] 헤더 포함 레이아웃 수정

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,42 +1,80 @@
+"use client";
 import Link from "next/link";
 import { ReactNode } from "react";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 interface MainLayoutProps {
   children: ReactNode;
 }
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  const pathname = usePathname();
+  const getActiveMenu = (url: string) => {
+    console.log(pathname);
+    return pathname === url ? "font-semibold underline" : "";
+  };
+
+  const menu = {
+    "/main": "Home",
+    "/main/users": "Users",
+    "/main/facilities": "Facilities",
+    "/main/administrators": "Administrators",
+  };
+
+  const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+  const handleOpenMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <div className="min-h-screen flex flex-col">
-      {/* Header */}
-      <header className="fixed top-0 left-0 w-full bg-neutral text-primary-content p-4 z-10 shadow-lg">
-        <div className="max-w-7xl mx-auto">
-          <h1 className="text-2xl font-bold">Spot Finder Admin</h1>
+      {/* NavBar */}
+      <div className="navbar bg-base-100">
+        <div className="flex-none">
+          <label
+            className="btn btn-square"
+            onClick={handleOpenMenu}
+            htmlFor="my-drawer"
+          >
+            <i className="ri-menu-line text-black h-4 w-4"></i>
+          </label>
         </div>
-      </header>
+        <div className="flex-1 ml-4">
+          <h1 className="text-2xl font-bold">
+            Spot Finder Admin
+            <span>{` > ${menu[pathname as keyof typeof menu]}`} </span>
+          </h1>
+        </div>
+      </div>
 
-      {/* Sidebar and Content */}
-      <div className="flex flex-1 pt-16">
-        {/* Sidebar */}
-        <nav className="fixed top-16 left-0 w-64 h-full shadow-lg">
-          <ul className="menu p-4 w-full space-y-2">
-            <li>
+      <div className="drawer">
+        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content">
+          {/* Page content here */}
+          <main className="p-8">{children}</main>
+        </div>
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+            <li className={getActiveMenu("/main")}>
               <Link href="/main">홈</Link>
             </li>
-            <li>
+            <li className={getActiveMenu("/main/users")}>
               <Link href="/main/users">사용자 관리</Link>
             </li>
-            <li>
+            <li className={getActiveMenu("/main/facilities")}>
               <Link href="/main/facilities">시설물 관리</Link>
             </li>
-            <li>
+            <li className={getActiveMenu("/main/administrators")}>
               <Link href="/main/administrators">관리자 관리</Link>
             </li>
           </ul>
-        </nav>
-
-        {/* Content */}
-        <main className="ml-64 p-8 flex-1 bg-base-200">{children}</main>
+        </div>
       </div>
     </div>
   );
