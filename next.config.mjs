07cbbdde9a91d6f8c380d98e76b9c9e61d42a304
Ye Won Lee <yewon.lee@seavantage.com>
2024-09-16 19:03:19 +0900

    [feat] next.config 이미지 host 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "spotfinder-image.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
