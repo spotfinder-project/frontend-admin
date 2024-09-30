6c33e747f31f72be4be776c6f33a894564e625b6
Ye Won Lee <yewon.lee@seavantage.com>
2024-09-30 15:18:46 +0900

    [impv] SWRconfig 수정

--- app/swrConfig.tsx
+++ app/swrConfig.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SWRConfig } from "swr";
+import { useRouter } from "next/navigation";
 
 interface ErrorResponse {
   status: "error";
@@ -14,12 +15,18 @@ type Props = {
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function SWRConfigContext({ children }: Props) {
+  const router = useRouter();
   return (
     <SWRConfig
       value={{
         fetcher,
         revalidateOnFocus: false, // Disable revalidation on focus globally
         shouldRetryOnError: false,
+        onError: (error) => {
+          if (error.status === 401) {
+            router.push("/");
+          }
+        },
       }}
     >
       {children}
